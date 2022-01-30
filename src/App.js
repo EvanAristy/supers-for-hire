3201c524cc5f2bf89b5f323d466bedc952f16b3b
EvanAristy <evanjaristy@gmail.com>
2022-01-30 18:22:48 -0500

    something with the links and router isnt working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { useState, useContext, useEffect } from 'react'
-// import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import axios from 'axios'
+// components
+import Nav from './components/Nav'
+// pages
+import Home from './pages/Home'
+import Login from './pages/Login'
+import HeroList from './pages/HeroList'
+// contexts
+// import UserContext from './contexts/UserContext'
 
 function App() {
 
+  const [user, setUser] = useState('')
   const [supersList, setSupersList] = useState([])
 
   useEffect(() => {
@@ -23,7 +32,17 @@ function App() {
 
   return (
     <div className="App">
-      
+      {/* <UserContext.Provider value={user}> */}
+
+        <Nav />
+
+        {/* <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='login' element={<Login />} />
+          <Route path='herolist' element={<HeroList />} />
+        </Routes> */}
+
+      {/* </UserContext.Provider> */}
     </div>
   );
 }
---
+++ src/pages/HeroList/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroList = () => {
+    return (
+        <div>
+            heroes
+        </div>
+    );
+}
+
+export default HeroList;
